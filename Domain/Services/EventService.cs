395b77bf54e3b70e352c684a4ace9e943d6ea1ae
Patryk Pochmara <01171335@pw.edu.pl>
2024-03-30 22:31:02 +0100

    Add example service

---
+++ Domain/Services/EventService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Abstractions;
+using Domain.DataModel;
+
+namespace Domain.Services;
+
+public class EventService
+{
+    private IUnitOfWork uow;
+
+    public EventService(IUnitOfWork uow)
+    {
+        this.uow = uow;
+    }
+
+    public Event? GetEvent(Guid guid) => uow.Repository<Event>().Get(guid);
+}
---
+++ UnitTests/Domain/Services/EventServicetTests.cs
@@ -0,0 +1,21 @@
+﻿using Domain.BaseTypes;
+using Domain.DataModel;
+using Infrastructure.UnitOfWorks;
+
+namespace UnitTests.Domain.Services;
+
+[TestClass]
+public class EventServicetTests
+{
+    [TestMethod]
+    public void GetEvent_Nonexistent_Null()
+    {
+        // TODO
+    }
+
+    [TestMethod]
+    public void GetEvent_Correct_Returns()
+    {
+        // TODO
+    }
+}

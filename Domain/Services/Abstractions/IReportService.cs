855e15bb2e93c17ae726d4d19169a210304bba53
Łukasz <luuxo77@gmail.com>
2024-04-10 00:54:05 +0200

    Namespace fix

--- Domain/Services/Abstractions/IReportService.cs
+++ Domain/Services/Abstractions/IReportService.cs
@@ -1,15 +1,14 @@
 ﻿using Domain.BaseTypes;
 using Domain.DataModel;
 
-namespace Domain.Services
+namespace Domain.Services;
+
+public interface IReportService
 {
-    public interface IReportService
-    {
-        ReportType CreateReport<TargetType, ReportType>(Guid targetId, Guid authorId, string title, string details, ReportCategory category)
-            where TargetType : BaseEntity
-            where ReportType : Report;
-        IEnumerable<ReportType> GetAll<ReportType>() where ReportType : Report;
-        ReportType GetByGuid<ReportType>(Guid guid) where ReportType : Report;
-        Report UpdateReport(Guid responderId, Guid reportId, string feedback, ReportState reportState);
-    }
+    ReportType CreateReport<TargetType, ReportType>(Guid targetId, Guid authorId, string title, string details, ReportCategory category)
+        where TargetType : BaseEntity
+        where ReportType : Report;
+    IEnumerable<ReportType> GetAll<ReportType>() where ReportType : Report;
+    ReportType GetByGuid<ReportType>(Guid guid) where ReportType : Report;
+    Report UpdateReport(Guid responderId, Guid reportId, string feedback, ReportState reportState);
 }

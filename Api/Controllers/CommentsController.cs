493ad145a50f2c387681deedfbd2903eb8c2d958
Łukasz <luuxo77@gmail.com>
2024-04-21 21:21:23 +0200

    Add CommentsController

---
+++ Api/Controllers/CommentsController.cs
@@ -0,0 +1,40 @@
+﻿using Api.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+// TODO: Implement controller
+[Route("comments")]
+[ApiController]
+public class CommentsController : ControllerBase
+{
+    /// <summary>
+    /// Creates a comment. There should be exactly one of postId or eventId fields present.
+    /// </summary>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult AddComment(CreateComment createComment)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Deletes a comment with given id. Users can delete only their own comments unless they have the ADMIN role.
+    /// </summary>
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult DeleteComment(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Searches for comments corresponding to given event or post. There should be exactly one of postId or eventId fields present.
+    /// </summary>
+    [HttpPost("search")]
+    [ProducesResponseType<PagedResponse<CommentDto>>(StatusCodes.Status200OK)]
+    public PagedResponse<CommentDto> SearchComments(EventSearchDetails details)
+    {
+        throw new NotImplementedException();
+    }
+}

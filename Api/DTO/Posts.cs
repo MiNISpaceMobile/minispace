1f0b13c557af42b193c4d1da0b9f74a7746e6b2b
Łukasz <luuxo77@gmail.com>
2024-04-21 19:48:00 +0200

    Add Posts DTOs

---
+++ Api/DTO/Posts.cs
@@ -0,0 +1,6 @@
+﻿namespace Api.DTO;
+
+public record PostDto(long Id, Guid Uuid, long EventId, long AuthorId,
+                      DateTime DatePosted, string Content);
+
+public record RegisterPost(long EventId, string Content, DateTime DatePosted);

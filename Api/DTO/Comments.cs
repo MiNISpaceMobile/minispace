ceb8c8b3df9f6321e8a512677679c24fa0e1b1c4
Łukasz <luuxo77@gmail.com>
2024-04-21 21:53:34 +0200

    Add DTO mapping (unfinished as not all needed entities and fields are present)

--- Api/DTO/Comments.cs
+++ Api/DTO/Comments.cs
@@ -1,6 +1,6 @@
 ﻿namespace Api.DTO;
 
-public record CommentDto(long EventId, long PostId, UserDto Author, string Content);
+public record CommentDto(long EventId, long PostId, UserDto? Author, string Content);
 
 // APISpec says that only one id should be present
 public record CommentSearchDetails(long? EventId, long? PostId, PageableRequest Pageable);
---
+++ Api/DTO/MappingExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Domain.DataModel;
+
+namespace Api.DTO;
+
+// Temporarily every Id is just 0 as we don't have Id in data model
+public static class MappingExtensions
+{
+    public static UserDto ToDto(this User user)
+    {
+        var role = user switch
+        {
+            Administrator => "ADMIN",
+            Student student => student.IsOrganizer ? "ORGANIZER" : null,
+            _ => null
+        };
+        var roles = role is null ? Enumerable.Empty<string>() : [role];
+        return new UserDto(default, user.FirstName, user.LastName, default, roles, default);
+    }
+
+    public static CommentDto ToDto(this Comment comment)
+    {
+        return new CommentDto(default, default, comment.Author?.ToDto(), comment.Content);
+    }
+
+    // Organizing unit is null as we don't have it in data model
+    public static EventDto ToDto(this Event @event)
+    {
+        return new EventDto(default, @event.Guid, @event.Title, null!, @event.StartDate,
+            @event.Description, @event.Participants.Count);
+    }
+
+    public static PostDto ToDto(this Post post)
+    {
+        return new PostDto(default, post.Guid, default, default, post.CreationDate, post.Content);
+    }
+}

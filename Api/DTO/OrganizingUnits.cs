4cd53c0f40b4ffad44d06a5cc07a850fda75a39d
Łukasz <luuxo77@gmail.com>
2024-04-21 20:15:33 +0200

    Add OrganizingUnits DTOs

---
+++ Api/DTO/OrganizingUnits.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.DTO;
+
+public record OrganizingUnitDto(long Id, string Name, long? ParentId);
+
+public record OrganizingUnitSearchResponse(long Id, string Name, long? ParentId)
+{
+    public bool isLeaf => ParentId == null;
+}
+
+

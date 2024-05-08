0f1f655ec244046f94eaedbcadadd77d7f2a918f
Patryk Pochmara <01171335@pw.edu.pl>
2024-05-08 14:55:32 +0200

    Add Swagger default parameter values

--- Api/SwaggerConfig.cs
+++ Api/SwaggerConfig.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.OpenApi.Models;
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
 
 namespace Api;
 
 public static class SwaggerConfig
 {
+    private static string ToCamelCase(this string name)
+        => char.ToLowerInvariant(name[0]) + name[1..];
+
     public static SwaggerGenOptions AddJwtAuthorization(this SwaggerGenOptions options)
     {
         options.AddSecurityDefinition("JWT", new OpenApiSecurityScheme
@@ -24,5 +29,49 @@ public static class SwaggerConfig
             }
         });
         return options;
+    }
+
+    public static SwaggerGenOptions AddDefaultValues(this SwaggerGenOptions options)
+    {
+        options.SchemaFilter<DefaultValueFilter>();
+        return options;
+    }
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
+    public class DefaultApiValueAttribute : Attribute
+    {
+        public IOpenApiPrimitive Value { get; set; }
+
+        public DefaultApiValueAttribute(string value)
+        {
+            Value = new OpenApiString(value);
+        }
+    }
+
+    public class DefaultValueFilter : ISchemaFilter
+    {
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            if (schema.Properties == null)
+                return;
+
+            foreach (ConstructorInfo cInfo in context.Type.GetConstructors())
+            {
+                foreach (ParameterInfo pInfo in cInfo.GetParameters())
+                {
+                    DefaultApiValueAttribute? defaultAttribute = pInfo.GetCustomAttribute<DefaultApiValueAttribute>();
+                    var value = defaultAttribute?.Value;
+                    if (value != null)
+                        schema.Properties[ToCamelCase(pInfo.Name!)].Example = value;
+                }
+            }
+            foreach (PropertyInfo pInfo in context.Type.GetProperties())
+            {
+                DefaultApiValueAttribute? defaultAttribute = pInfo.GetCustomAttribute<DefaultApiValueAttribute>();
+                var value = defaultAttribute?.Value;
+                if (value != null)
+                    schema.Properties[ToCamelCase(pInfo.Name!)].Example = value;
+            }
+        }
     }
 }

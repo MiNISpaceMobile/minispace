cce7103f59642d10c0ec1aa8f85300d661d39187
Patryk Pochmara <01171335@pw.edu.pl>
2024-05-29 19:49:19 +0200

    Add Reaction to data model

--- Domain/DataModel/Post.cs
+++ Domain/DataModel/Post.cs
@@ -2,6 +2,40 @@
 
 namespace Domain.DataModel;
 
+public enum ReactionType
+{
+    Unknown,
+    Like,
+    Funny,
+    Wow,
+    Angry,
+    Sad,
+}
+
+public class Reaction
+{
+    public Guid AuthorId { get; private set; }
+    public virtual User Author { get; set; }
+
+    public Guid PostId { get; protected set; }
+    public virtual Post Post { get; set; }
+
+    public ReactionType Type { get; set; }
+
+#pragma warning disable CS8618 // Unassigned non-nullables
+    protected Reaction() { }
+#pragma warning restore CS8618 // Unassigned non-nullables
+
+    public Reaction(User author, Post post, ReactionType type)
+    {
+        Author = author;
+
+        Post = post;
+
+        Type = type;
+    }
+}
+
 public class Post : BaseEntity
 {
     public Guid? AuthorId { get; private set; }
@@ -15,6 +49,7 @@ public class Post : BaseEntity
 
     public virtual ICollection<Comment> Comments { get; }
     public virtual ICollection<PostPicture> Pictures { get; }
+    public virtual ICollection<Reaction> Reactions { get; }
 
 #pragma warning disable CS8618 // Unassigned non-nullables
     protected Post() { }
@@ -32,5 +67,6 @@ public class Post : BaseEntity
         
         Comments = new List<Comment>();
         Pictures = new List<PostPicture>();
+        Reactions = new List<Reaction>();
     }
 }
